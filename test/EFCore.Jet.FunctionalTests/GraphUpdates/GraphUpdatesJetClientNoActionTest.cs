a9ef005b2c4a573eaa86edfcb26c9dbd5f3bfe38
Christopher Jolly <chris-jolly_au@hotmail.com>
2024-08-27 00:52:37 +0800

    Add the GraphUpdates tests in

---
+++ test/EFCore.Jet.FunctionalTests/GraphUpdates/GraphUpdatesJetClientNoActionTest.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+public class GraphUpdatesJetClientNoActionTest(GraphUpdatesJetClientNoActionTest.JetFixture fixture) : GraphUpdatesJetTestBase<
+    GraphUpdatesJetClientNoActionTest.JetFixture>(fixture)
+{
+    protected override void UseTransaction(DatabaseFacade facade, IDbContextTransaction transaction)
+        => facade.UseTransaction(transaction.GetDbTransaction());
+
+    public class JetFixture : GraphUpdatesJetFixtureBase
+    {
+        public override bool ForceClientNoAction
+            => true;
+
+        protected override string StoreName
+            => "GraphClientNoActionUpdatesTest";
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
+        {
+            base.OnModelCreating(modelBuilder, context);
+
+            foreach (var foreignKey in modelBuilder.Model
+                         .GetEntityTypes()
+                         .SelectMany(e => e.GetDeclaredForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.ClientNoAction;
+            }
+        }
+    }
+}
--- test/EFCore.Jet.FunctionalTests/GraphUpdates/ProxyGraphUpdatesJetTest.cs
+++ test/EFCore.Jet.FunctionalTests/GraphUpdates/ProxyGraphUpdatesJetTest.cs
@@ -1,118 +1,180 @@
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Threading.Tasks;
 using EntityFrameworkCore.Jet.FunctionalTests.TestUtilities;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.TestUtilities;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace EntityFrameworkCore.Jet.FunctionalTests
+namespace Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+public abstract class ProxyGraphUpdatesJetTest
 {
-    public abstract class ProxyGraphUpdatesJetTest
+    public abstract class ProxyGraphUpdatesJetTestBase<TFixture> : ProxyGraphUpdatesTestBase<TFixture>
+        where TFixture : ProxyGraphUpdatesJetTestBase<TFixture>.ProxyGraphUpdatesJetFixtureBase, new()
     {
-        public abstract class ProxyGraphUpdatesJetTestBase<TFixture> : ProxyGraphUpdatesTestBase<TFixture>
-            where TFixture : ProxyGraphUpdatesJetTestBase<TFixture>.ProxyGraphUpdatesJetFixtureBase, new()
+        protected ProxyGraphUpdatesJetTestBase(TFixture fixture)
+            : base(fixture)
         {
-            protected ProxyGraphUpdatesJetTestBase(TFixture fixture)
-                : base(fixture)
-            {
-            }
-
-            protected override void UseTransaction(DatabaseFacade facade, IDbContextTransaction transaction)
-                => facade.UseTransaction(transaction.GetDbTransaction());
-
-            public abstract class ProxyGraphUpdatesJetFixtureBase : ProxyGraphUpdatesFixtureBase
-            {
-                public TestSqlLoggerFactory TestSqlLoggerFactory => (TestSqlLoggerFactory)ListLoggerFactory;
-                protected override ITestStoreFactory TestStoreFactory => JetTestStoreFactory.Instance;
-            }
         }
 
-        public class LazyLoading : ProxyGraphUpdatesJetTestBase<LazyLoading.ProxyGraphUpdatesWithLazyLoadingJetFixture>
+        protected override void UseTransaction(DatabaseFacade facade, IDbContextTransaction transaction)
+            => facade.UseTransaction(transaction.GetDbTransaction());
+
+        public abstract class ProxyGraphUpdatesJetFixtureBase : ProxyGraphUpdatesFixtureBase
         {
-            public LazyLoading(ProxyGraphUpdatesWithLazyLoadingJetFixture fixture)
-                : base(fixture)
+            public TestSqlLoggerFactory TestSqlLoggerFactory
+                => (TestSqlLoggerFactory)ListLoggerFactory;
+
+            protected override ITestStoreFactory TestStoreFactory
+                => JetTestStoreFactory.Instance;
+        }
+    }
+
+    public class LazyLoading(LazyLoading.ProxyGraphUpdatesWithLazyLoadingJetFixture fixture)
+        : ProxyGraphUpdatesJetTestBase<LazyLoading.ProxyGraphUpdatesWithLazyLoadingJetFixture>(fixture)
+    {
+        protected override bool DoesLazyLoading
+            => true;
+
+        protected override bool DoesChangeTracking
+            => false;
+
+        public class ProxyGraphUpdatesWithLazyLoadingJetFixture : ProxyGraphUpdatesJetFixtureBase
+        {
+            protected override string StoreName
+                => "ProxyGraphLazyLoadingUpdatesTest";
+
+            public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+                => base.AddOptions(builder.UseLazyLoadingProxies());
+
+            protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
+                => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
             {
-            }
+                modelBuilder.UseJetIdentityColumns();
 
-            protected override bool DoesLazyLoading => true;
-            protected override bool DoesChangeTracking => false;
+                base.OnModelCreating(modelBuilder, context);
 
-            public class ProxyGraphUpdatesWithLazyLoadingJetFixture : ProxyGraphUpdatesJetFixtureBase
-            {
-                protected override string StoreName { get; } = "ProxyGraphLazyLoadingUpdatesTest";
-
-                public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
-                    => base.AddOptions(builder.UseLazyLoadingProxies());
-
-                protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
-                    => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
-
-                protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
-                {
-                    modelBuilder.UseIdentityColumns();
-
-                    base.OnModelCreating(modelBuilder, context);
-                }
+                //custom long to int
+                modelBuilder.Entity<SharedFkRoot>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
+                modelBuilder.Entity<SharedFkParent>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
+                modelBuilder.Entity<SharedFkDependant>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
             }
         }
+    }
 
-        public class ChangeTracking : ProxyGraphUpdatesJetTestBase<ChangeTracking.ProxyGraphUpdatesWithChangeTrackingJetFixture>
+    public class ChangeTracking(ChangeTracking.ProxyGraphUpdatesWithChangeTrackingJetFixture fixture)
+        : ProxyGraphUpdatesJetTestBase<ChangeTracking.ProxyGraphUpdatesWithChangeTrackingJetFixture>(fixture)
+    {
+        // Needs lazy loading
+        public override Task Save_two_entity_cycle_with_lazy_loading()
+            => Task.CompletedTask;
+
+        protected override bool DoesLazyLoading
+            => false;
+
+        protected override bool DoesChangeTracking
+            => true;
+
+        public class ProxyGraphUpdatesWithChangeTrackingJetFixture : ProxyGraphUpdatesJetFixtureBase
         {
-            public ChangeTracking(ProxyGraphUpdatesWithChangeTrackingJetFixture fixture)
-                : base(fixture)
+            protected override string StoreName
+                => "ProxyGraphChangeTrackingUpdatesTest";
+
+            public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+                => base.AddOptions(builder.UseChangeTrackingProxies());
+
+            protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
+                => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
             {
-            }
+                modelBuilder.UseJetIdentityColumns();
 
-            protected override bool DoesLazyLoading => false;
-            protected override bool DoesChangeTracking => true;
+                base.OnModelCreating(modelBuilder, context);
 
-            public class ProxyGraphUpdatesWithChangeTrackingJetFixture : ProxyGraphUpdatesJetFixtureBase
-            {
-                protected override string StoreName { get; } = "ProxyGraphChangeTrackingUpdatesTest";
-
-                public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
-                    => base.AddOptions(builder.UseChangeTrackingProxies());
-
-                protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
-                    => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
-
-                protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
-                {
-                    modelBuilder.UseIdentityColumns();
-
-                    base.OnModelCreating(modelBuilder, context);
-                }
+                //custom long to int
+                modelBuilder.Entity<SharedFkRoot>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
+                modelBuilder.Entity<SharedFkParent>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
+                modelBuilder.Entity<SharedFkDependant>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
             }
         }
+    }
 
-        public class ChangeTrackingAndLazyLoading : ProxyGraphUpdatesJetTestBase<ChangeTrackingAndLazyLoading.ProxyGraphUpdatesWithChangeTrackingAndLazyLoadingJetFixture>
+    public class ChangeTrackingAndLazyLoading(
+        ChangeTrackingAndLazyLoading.ProxyGraphUpdatesWithChangeTrackingAndLazyLoadingJetFixture fixture)
+        : ProxyGraphUpdatesJetTestBase<
+            ChangeTrackingAndLazyLoading.ProxyGraphUpdatesWithChangeTrackingAndLazyLoadingJetFixture>(fixture)
+    {
+        protected override bool DoesLazyLoading
+            => true;
+
+        protected override bool DoesChangeTracking
+            => true;
+
+        public class ProxyGraphUpdatesWithChangeTrackingAndLazyLoadingJetFixture : ProxyGraphUpdatesJetFixtureBase
         {
-            public ChangeTrackingAndLazyLoading(ProxyGraphUpdatesWithChangeTrackingAndLazyLoadingJetFixture fixture)
-                : base(fixture)
+            protected override string StoreName
+                => "ProxyGraphChangeTrackingAndLazyLoadingUpdatesTest";
+
+            public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+                => base.AddOptions(builder.UseLazyLoadingProxies().UseChangeTrackingProxies());
+
+            protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
+                => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
             {
-            }
+                modelBuilder.UseJetIdentityColumns();
 
-            protected override bool DoesLazyLoading => true;
-            protected override bool DoesChangeTracking => true;
+                base.OnModelCreating(modelBuilder, context);
 
-            public class ProxyGraphUpdatesWithChangeTrackingAndLazyLoadingJetFixture : ProxyGraphUpdatesJetFixtureBase
-            {
-                protected override string StoreName { get; } = "ProxyGraphChangeTrackingAndLazyLoadingUpdatesTest";
-
-                public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
-                    => base.AddOptions(builder.UseLazyLoadingProxies().UseChangeTrackingProxies());
-
-                protected override IServiceCollection AddServices(IServiceCollection serviceCollection)
-                    => base.AddServices(serviceCollection.AddEntityFrameworkProxies());
-
-                protected override void OnModelCreating(ModelBuilder modelBuilder, DbContext context)
-                {
-                    modelBuilder.UseIdentityColumns();
-
-                    base.OnModelCreating(modelBuilder, context);
-                }
+                //custom long to int
+                modelBuilder.Entity<SharedFkRoot>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
+                modelBuilder.Entity<SharedFkParent>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
+                modelBuilder.Entity<SharedFkDependant>(
+                    b =>
+                    {
+                        b.Property(e => e.Id).HasColumnType("counter");
+                    });
             }
         }
     }
